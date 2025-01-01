316f327a8c1c42a8dce0ffc504cafd2f43940e98
mayank2130 <mayankthakur1712@gmail.com>
2025-01-01 22:51:35 +0530

    components border

--- src/components/ui-components/index.tsx
+++ src/components/ui-components/index.tsx
@@ -9,7 +9,7 @@ const UiComponents = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-x-10 h-full ml-10 mt-20">
         {componentsData.map((component) => (
           <Link className="mt-10 mb-10" href={component.link} key={component.name}>
-            <Card className="flex flex-col items-center justify-center bg-black h-72 border border-[#282727]">
+            <Card className="flex flex-col items-center justify-center bg-black h-72 border border-[#FFDB89]">
               <CardContent>
                 <Image
                   src={component.imageSrc}
